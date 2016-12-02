6b56aa5b7c83cff11c004d327cf6015c54838538
George Bryant <george.s.bryant@gmail.com>
2016-12-02 16:05:56 +0000

    Correct syntax of before and after commands

--- tests/demoTest.js
+++ tests/demoTest.js
@@ -1,30 +1,36 @@
+// let before = function (browser) {
+//   console.log('Setting up...');
+// };
+//
+// let after = function (browser) {
+//   console.log("Closing down...");
+// };
+//
+// let testSteps = 1;
+// let beforeEach = function (browser, done) {
+//   console.log(`Executing step ${testSteps}...`);
+//   done();
+// };
+//
+// let afterEach = function(browser, done) {
+//   console.log(`Step ${testSteps} complete...`);
+//   testSteps = testSteps+1;
+//   done();
+// };
+
 module.exports = {
 
-  // before : function (browser) {
-  //   console.log("Setting up...");
-  // }
-  //
-  // after : function (browser) {
-  //   console.log("Closing down...");
-  //};
-  //
-  // let testSteps = 1
-  // beforeEach : function (browser) {
-  //   console.log("Executing step "+testSteps+"...");
-  // }
-  //
-  // afterEach : function () {
-  //   console.log("Step "+testSteps+" complete...");
-  //   testSteps = testSteps+1;
-  // }
-
+  // before,
+  // after,
+  // beforeEach,
+  // afterEach,
   'Demo test Google' : function (browser) {
     browser.url('http://www.google.com')
     browser.waitForElementVisible('body', 1000)
     browser.setValue('input[type=text]', 'nightwatch')
     browser.waitForElementVisible('button[name=btnG]', 1000)
     browser.click('button[name=btnG]')
-    browser.pause(1000)
+    browser.pause(2000)
     browser.assert.containsText('#main', 'Night Watch')
     browser.end();
   }
