e0c66c3d015c8135e7fe1113f3ed82bc4dc036b8
George Bryant <george.s.bryant@gmail.com>
2016-12-05 16:10:44 +0000

    Replace switch statement with object literals and impelement template scripts that works on the JSON objects that will be pulled from the database

--- tests/template.js
+++ tests/template.js
@@ -1,51 +1,56 @@
-//Not sure if this will work, more work on the databases side needed before I can tell
-
-//Commenting out as it is not the standard scipt format, so is causing errors
-// module.exports = function (data) {
-//   return {
-//     '@disabled': true,
-//     testID: function(browser) {
-//       browser
-//         .url(data.url)
-//         .waitForElementVisible(data.element)
-//         .assert.title(data.title)
-//         .saveScreenshot(data.screenshot)
-//         .end();
-//   }
-// }
-
-let data = {
-    url: 'http://www.google.com',
-    idOrClass: 'Class',
-    elementID: '_XIi',
-    action: 'Click',
-    value: '',
-    done: 'false'
+//Test data until we can start pulling from the database
+let rawData = { url: 'https://xes.io/contact/',
+  done: 'false',
+  steps: `[{"id":"ID","element":"name","action":"Click","value":""},
+  {"id":"ID","element":"name","action":"TextEntry","value":"George Bryant"},
+  {"id":"Class","element":"faint","action":"Assert","value": "Tell me all about your project. Include details such as schedule, budget and example sites you like."}]`
 }
 
-this.templateTest = (browser) => {
-  browser.url(data.url);
-  browser.waitForElementVisible('body', 1000);
-  switch(data.action) {
-    case "Click":
-      if (data.idOrClass === 'ID') {
-        browser.click(`#${data.elementID}`);
-      } else {
-        browser.click(`.${data.elementID}`);
-      }
-      break;
 
-    case "Text Entry":
-      if (data.idOrClass === 'ID') {
+let testSteps = JSON.parse(rawData.steps);
+let totalSteps = testSteps.length;
 
-      } else {
-
-      }
-      break;
-
-    case "Assert":
-      break;
+//Click on the element
+let clickCallback = (data, browser) => {
+  if (data.id === 'ID') {
+    browser.click(`#${data.element}`);
+  } else {
+    browser.click(`.${data.element}`);
   }
-  browser.pause(20000);
+};
+
+//Enter text into the element
+let textEntryCallback = (data, browser) => {
+  if (data.id === 'ID') {
+    browser.setValue(`#${data.element}`, `${data.value}`);
+  } else {
+    browser.setValue(`.${data.element}`, `${data.value}`);
+  }
+}
+
+//Assert the element contains text
+let assertCallback = (data, browser) => {
+  if (data.id === 'ID') {
+    browser.assert.containsText(`#${data.element}`, `${data.value}`);
+  } else {
+    browser.assert.containsText(`.${data.element}`, `${data.value}`);
+  }
+}
+
+//Test script that will be used
+this.templateTest = (browser) => {
+  browser.url(rawData.url);
+  browser.waitForElementVisible('body', 1000);
+
+  let actions = {
+    'Click': clickCallback,
+    'TextEntry': textEntryCallback,
+    'Assert': assertCallback,
+  };
+  browser.pause(500);
+  testSteps.forEach(step => {
+    actions[step.action](step, browser);
+    browser.pause(500);
+  });
   browser.end();
 }
