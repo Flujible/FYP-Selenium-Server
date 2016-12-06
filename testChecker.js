83c604b6e2f812cb86b52be6a7a2d542b7aec834
George Bryant <george.s.bryant@gmail.com>
2016-12-06 15:55:07 +0000

    testChecker file now reads the key and value from redis database

--- testChecker.js
+++ testChecker.js
@@ -1,15 +1,35 @@
+let redisClient = require('redis').createClient(process.env.REDIS_URL);
 let nightwatch = require('nightwatch');
 
 
-// redisClient.keys('*', function (keys) {
+redisClient.on('connect', () => {
+  console.log("Redis connected")
+})
+
+redisClient.keys('*', function (err, keys) {
+  if (err) {return console.log(err);}
+  let keyArray = keys;
+  console.log(keys);
+
+  keys.forEach(key => {
+    redisClient.hgetall(key, (err, object) => {
+      let value = object;
+      console.log("\n");
+      console.log(value);
+    });
+  });
+
 //   keys.forEach(key => {
-//     redisClient.get(key, function (result) {
-//
+//     redisClient.hgetall(key, function (result) {
+//       let testArray = JSON.parse(result);
+//       console.log("\n\n" + testArray + "\n\n");
+//       if (testArray.done === false) {
+//         nightwatch.runner({
+//           config: "./nightwatch.conf.js"
+//         }, result);
+//       }
 //     })
 //   })
-// })
 
 
-nightwatch.runner({
-  config: "./nightwatch.conf.js"
-});
+})
