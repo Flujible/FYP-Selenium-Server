ffb281ce7d1e7fc88def8da5d0484d58e9ce156a
George Bryant <george.s.bryant@gmail.com>
2016-12-08 11:10:29 +0000

    Remove attempts to programatically call nightwatch with test ID

--- testChecker.js
+++ testChecker.js
@@ -1,23 +1,36 @@
-let redisClient = require('redis').createClient(process.env.REDIS_URL);
 let nightwatch = require('nightwatch');
 
+nightwatch.runner({
+  config: "./nightwatch.conf.js"
+});
 
-redisClient.on('connect', () => {
-  console.log("Redis connected")
-})
 
-redisClient.keys('*', function (err, keys) {
-  if (err) {return console.log(err);}
-  let keyArray = keys;
-  console.log(keys);
+// I wanted to try and scan the redis database and only call nightwatch if there
+// was a test to be executed, but I can not work out how to do this without
+// modifying some of the source code for Nightwatch, so I will not implement
+//
+//
+// let redisClient = require('redis').createClient(process.env.REDIS_URL);
+// let nightwatch = require('nightwatch');
+//
+//
+// redisClient.on('connect', () => {
+//   console.log("Redis connected")
+// })
+//
+// redisClient.keys('*', function (err, keys) {
+//   if (err) {return console.log(err);}
+//   let keyArray = keys;
+//   console.log(keys);
+//
+//   keys.forEach(key => {
+//     redisClient.hgetall(key, (err, object) => {
+//       let value = object;
+//       console.log("\n");
+//       console.log(value);
+//     });
+//   });
 
-  keys.forEach(key => {
-    redisClient.hgetall(key, (err, object) => {
-      let value = object;
-      console.log("\n");
-      console.log(value);
-    });
-  });
 
 //   keys.forEach(key => {
 //     redisClient.hgetall(key, function (result) {
@@ -32,4 +45,4 @@ redisClient.keys('*', function (err, keys) {
 //   })
 
 
-})
+//})
