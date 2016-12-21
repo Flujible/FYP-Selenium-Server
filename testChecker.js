0610f28e834f4ef649244bb15c8f576c98e23896
George Bryant <george.s.bryant@gmail.com>
2016-12-21 14:31:25 +0000

    Remove unneeded code

--- testChecker.js
+++ testChecker.js
@@ -34,34 +34,3 @@ redisClient.keys('*', function (err, keys) {
     });
   });
 });
-// nightwatch.runner({
-//   config: "./nightwatch.conf.js"
-// });
-
-
-// I wanted to try and scan the redis database and only call nightwatch if there
-// was a test to be executed, but I can not work out how to do this without
-// modifying some of the source code for Nightwatch, so I will not implement
-//
-//
-// let redisClient = require('redis').createClient(process.env.REDIS_URL);
-// let nightwatch = require('nightwatch');
-//
-//
-
-
-
-//   keys.forEach(key => {
-//     redisClient.hgetall(key, function (result) {
-//       let testArray = JSON.parse(result);
-//       console.log("\n\n" + testArray + "\n\n");
-//       if (testArray.done === false) {
-//         nightwatch.runner({
-//           config: "./nightwatch.conf.js"
-//         }, result);
-//       }
-//     })
-//   })
-
-
-//})
