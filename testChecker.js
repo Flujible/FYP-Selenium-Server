be40a068dd97815be0792bf733d59c65de20b928
George Bryant <george.s.bryant@gmail.com>
2016-12-22 14:55:56 +0000

    Split the file writing and nightwatch execution into bespoke functions but this has not helped

--- testChecker.js
+++ testChecker.js
@@ -6,31 +6,42 @@ redisClient.on('connect', () => {
   console.log("Redis connected")
 });
 
+
+
+let writeFile = (key) => {
+  //Get the value from a specified redis key and assign it to 'object'
+  redisClient.hgetall(key, (err, object) => {
+    let value = object;
+    console.log(value);
+
+    //Write the steps outlined in the redis database to a JSON file
+    if (value.done === 'false') {
+      value.steps = JSON.parse(value.steps);
+      fs.writeFile("tests/data.json", JSON.stringify(value, null, 4), 'utf8',function(err) {
+        if (err) {
+          return console.error(err);
+        }
+        console.log("File created successfully");
+      });
+
+    }
+  });
+}
+
+//Run nightwatch and set the 'done' value to true
+let exec = (key) => {
+  nightwatch.runner({config: "./nightwatch.conf.js"});
+  redisClient.hmset(key, done, "true");
+};
+
+
+
 redisClient.keys('*', function (err, keys) {
   if (err) {return console.log(err);}
-  let keyArray = keys;
   console.log(keys);
 
-  keys.forEach(key => {
-    redisClient.hgetall(key, (err, object) => {
-      let value = object;
-      console.log("\n");
-      console.log(value);
-      if (value.done === 'false') {
-        value.steps = JSON.parse(value.steps);
-        fs.writeFile("tests/data.json", JSON.stringify(value, null, 4), 'utf8',function(err) {
-          if (err) {
-            return console.error(err);
-          }
-          console.log("File created successfully");
-
-        });
-        //This may need a setTimout if the files get written to quickly
-        nightwatch.runner({config: "./nightwatch.conf.js"});
-        //
-        // Need to set done to true here
-        //
-      }
-    });
-  });
+  for (let i = 0; i < keys.length; i++) {
+    writeFile(keys[i]);
+    exec(keys[i]);
+  }
 });
