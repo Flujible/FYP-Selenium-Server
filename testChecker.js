cfdbb43f61f5dcf33cf752171f4d2d7c0ce5feca
George Bryant <george.s.bryant@gmail.com>
2017-01-06 16:56:22 +0000

    Changed the approach to now use promises in order to execute the test scripts

--- testChecker.js
+++ testChecker.js
@@ -1,53 +1,57 @@
-let nightwatch = require('nightwatch');
 let redisClient = require('redis').createClient(process.env.REDIS_URL);
+let nightwatch = require('nightwatch');
 let fs = require('fs');
-let asyn = require('async');
-let sleep = require('sleep');
 
 redisClient.on('connect', () => {
   console.log("Redis connected")
 });
 
+let writeKey = function (key) {
+  return new Promise(function(resolve, reject) {
+    redisClient.hgetall(key, (err, object) => {
+      let value = object;
 
-
-let writeFile = (key) => {
-  //Get the value from a specified redis key and assign it to 'object'
-  redisClient.hgetall(key, (err, object) => {
-    let value = object;
-    console.log(value);
-
-    //Write the steps outlined in the redis database to a JSON file
-    if (value.done === 'false') {
-      value.steps = JSON.parse(value.steps);
-      fs.writeFile("tests/data.json", JSON.stringify(value, null, 4), 'utf8',function(err) {
-        if (err) {
-          return console.error(err);
+      //Write the steps outlined in the redis database to a JSON file
+      if (value.done === 'false') {
+        value.steps = JSON.parse(value.steps);
+        fs.writeFile(`tests/${key}.js`,
+          `module.exports = { '${key}' : require('../testUtils')(${JSON.stringify(value, null, 4)})};`
+          , 'utf8', err => {
+            if (err) {
+              return reject(err);
+            } else {
+              return resolve(true);
+            }
+          });
+        } else {
+          return Promise.resolve();
         }
-        console.log("++++++++++ File created successfully ++++++++++");
       });
 
-    }
   });
-}
-
-//Run nightwatch and set the 'done' value to true
-let exec = (key) => {
-  nightwatch.runner({config: "./nightwatch.conf.js"});
-  redisClient.hmset(key, done, "true");
 };
 
+let writeKeys = keys => {
+  let inProgress = [];
 
+  keys.forEach(key => {
+    inProgress.push(writeKey(key));
+  });
 
+  return Promise.all(inProgress);
+};
+
+let runTests = () => {
+  let config = './nightwatch.conf.js';
+  nightwatch.runner({ config }, () => {
+    console.log('Done');
+    process.exit(0);
+  });
+};
+
+// Get the Redis keys and store them in an array
 redisClient.keys('*', function (err, keys) {
   if (err) {return console.log(err);}
   console.log(keys);
-
-  for (let i = keys.length; i > 0; i--) {
-    asyn.series([
-      writeFile(keys[i]),
-      sleep.sleep(15),
-      exec(keys[i]),
-      sleep.sleep(10)
-    ]);
-  }
+  writeKeys(keys).then(runTests);
 });
