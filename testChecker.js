96750725a7facac8e87e74e28f2820cfe1e4cd35
George Bryant <george.s.bryant@gmail.com>
2017-02-08 17:12:32 +0000

    Resolve jshint errors and update debugging messages for more concise outputs

--- testChecker.js
+++ testChecker.js
@@ -7,7 +7,7 @@ let parser = new xml2js.Parser();
 
 //Connect to the redis DB
 redisClient.on('connect', () => {
-  console.log("Redis connected");
+  console.log(":: Connected to redis");
 });
 
 //Returns a promise which resolves when the files are written and rejects if they dont
@@ -15,14 +15,15 @@ let writeKey = function (key) {
   return new Promise(function(resolve, reject) {
     //Gets all the information from the key it was passed
     redisClient.hgetall(key, (err, object) => {
+      if (err) return reject(err);
       let value = object;
 
       //Write the steps outlined in the redis database to a JSON file
         value.steps = JSON.parse(value.steps);
         if (value.status === 'pending') {
+          console.log(`:: Creating test script for ${key}`);
         fs.writeFile(`tests/${key}.js`,
-          `module.exports = { '${key}' : require('../testUtils')(${JSON.stringify(value, null, 4)})};`
-          , 'utf8', err => {
+          `module.exports = { '${key}' : require('../testUtils')(${JSON.stringify(value, null, 4)})};`, 'utf8', err => {
             if (err) {
               return reject(err);
             } else {
@@ -49,56 +50,54 @@ let writeKeys = keys => {
 
 //Executes nightwatch
 let runTests = (keys) => {
-  return new Promise(function(resolve, reject) {
+  return new Promise(function(resolve) {
     keys = keys.filter(key => typeof key !== 'undefined');
     if (keys.length > 0) {
       let config = './nightwatch.conf.js';
+      nightwatch = require('nightwatch');
+      console.log(":: Running Nightwatch");
       nightwatch.runner({ config }, () => {
-        console.log(':: Done');
+        console.log(':: Nightwatch finished');
         return resolve(keys);
       });
     } else {
-      console.log(':: No tests to run, exiting');
+      console.log(':: No tests to run');
       process.exit();
     }
   });
 };
 
 let cleanUp = keys => {
-  console.log(":: cleanUp function");
+  console.log(":: Cleaning up keys");
   return Promise.all(keys.map(purgeKey));
 };
 
 
 let purgeKey = key => {
   return new Promise(function(resolve, reject) {
-    console.log(`:: Marking ${key} as done`);
     fs.readdir("reports", (err, files) => {
-      console.log(`:: fs.readdir initiated`);
       if(err) {
         console.log(`:: Error finding reports folder`);
         console.error(err);
       }
-      console.log(`:: Folder found`);
       files.forEach(file => {
         if (file.includes(key)) {
-          console.log(`:: About to read file`);
           fs.readFile(`reports/${file}`, 'utf-8', (err, data) => {
             if (err) return reject(err);
             parser.parseString(data, (err, result) => {
               if(err) return reject(err);
-              if (result.testsuites['$'].errors === '0' && result.testsuites['$'].failures === '0') {
-                redisClient.hset(key, "status", "successful", function(err, _) {
+              if (result.testsuites.$.errors === '0' && result.testsuites.$.failures === '0') {
+                redisClient.hset(key, "status", "successful", function(err) {
                   if(err) return reject(err);
                   fs.unlink(`tests/${key}.js`);
-                  console.log(`:: Resolving on successful`);
+                  console.log(`:: Set to successful: ${key}`);
                   return resolve(key);
                 });
               } else {
-                redisClient.hset(key, "status", "failed", function(err, _) {
+                redisClient.hset(key, "status", "failed", function(err) {
                   if(err) return reject(err);
                   fs.unlink(`tests/${key}.js`);
-                  console.log(`:: Resolving on failed`);
+                  console.log(`:: Set to failed: ${key}`);
                   return resolve(key);
                 });
               }
@@ -119,6 +118,7 @@ let writeResults = keys => {
 
   //This will return a promise which resolves to true if all of the promises in
   //the inProgress array resolve, or if any of them fail, it will reject
+  console.log(":: Writing results to redis");
   return Promise.all(inProgress);
 };
 
@@ -128,15 +128,13 @@ let writeResult = key => {
       if(err) console.error(err);
       files.forEach(file => {
         if (file.includes(key)) {
-          console.log(`:: File ${file} contains key`);
           fs.readFile(`reports/${file}`, 'utf-8', (err, data) => {
             if (err) {
-              console.log(`:: Rejecting ${key}`);
+              console.log(`:: Could not write result for ${key}`);
               return reject(err);
             } else {
-              redisClient.hset(key, "result", data, function(err, _) {
+              redisClient.hset(key, "result", data, function(err) {
                 if(err) {return reject(err);}
-                console.log(`:: Writing result - resolving ${key}`);
                 return resolve(key);
               });
             }
