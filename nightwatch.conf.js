064eba0af8fcc2f60de91e2a50fb16fbc40bba6c
George Bryant <george.bryant@heathwallace.com>
2017-07-21 15:34:28 +0000

    added quotes and args for running chrome headless

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,32 +1,35 @@
 module.exports = {
-  src_folders: "tests", //Where the tests are stored
-  output_folder: "reports", //Where the test results are stored
-  selenium: { //allows us to define how selenium will run
-    start_process: true, //Nightwatch itself will be starting the selenium server
-    server_path: "./node_modules/nightwatch/bin/selenium.jar",
-    host: "127.0.0.1",
-    port: "4444",
-    cli_args: {
-      "webdriver.chrome.driver": "./node_modules/nightwatch/bin/chromedriver"
+  "src_folders": "tests", //Where the tests are stored
+  "output_folder": "reports", //Where the test results are stored
+  "selenium": { //allows us to define how selenium will run
+    "start_process": true, //Nightwatch itself will be starting the selenium server
+    "server_path": "./bin/selenium.jar",
+    "host": "127.0.0.1",
+    "port": 4444,
+    "cli_args": {
+      "webdriver.chrome.driver": "./bin/chromedriver"
     }
   },
-  test_settings: {
-    'default': {
-      screenshots: {
-        enabled: false, //No screenshots please
-        path: "./screenshots" //where to store screenshots if desired
+  "test_settings": {
+    "default": {
+      "screenshots": {
+        "enabled": false, //No screenshots please
+        "path": "./screenshots" //where to store screenshots if desired
       },
-      globals: {
-        waitForConditionTimeout: 5000 //prevent the connection from timing out if the internet is slow
+      "globals": {
+        "waitForConditionTimeout": 5000 //prevent the connection from timing out if the internet is slow
       },
-      desiredCapabilities: {
-        browserName: "chrome" //Default browser should be chrome
+      "desiredCapabilities": {
+        "browserName": "chrome", //Default browser should be chrome,
+	"chromeOptions": {
+	  "args": ["headless", "disable-gpu"]
+	}
       }
     },
-    chrome: { //settings for the default browser
-      desiredCapabilities: {
-        browserName: "chrome",
-        javascriptEnabled: true
+    "chrome": { //settings for the default browser
+      "desiredCapabilities": {
+        "browserName": "chrome",
+        "javascriptEnabled": true
       }
     }
   }
